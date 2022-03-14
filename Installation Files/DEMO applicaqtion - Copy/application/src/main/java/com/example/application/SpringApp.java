3fe2b19e88b82435c27cb6901e1ffb7195a4b788
Revelmonger <cdmann0288@ung.edu>
2022-03-14 15:13:05 -0400

    Update
    ~chase

---
+++ Installation Files/DEMO applicaqtion - Copy/application/src/main/java/com/example/application/SpringApp.java
@@ -0,0 +1,13 @@
+package com.example.application;
+
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringApp {
+    public static void main(String[] args) {
+        Application.launch(FXApp.class, args);
+    }
+}
