7e5e1091d2d1a745edc451189bf00bb70cdf7670
Revelmonger <cdmann0288@ung.edu>
2022-03-15 17:21:12 -0400

    ~update
    Attemtping database connectin

---
+++ Installation Files/DEMO application - Copy/application/src/main/java/com/example/application/Controller.java
@@ -0,0 +1,47 @@
+package com.example.application;
+
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button Login_ButtonID;
+
+    @FXML
+    private TextField UsernameFieldID;
+
+    @FXML
+    private PasswordField PasswordFieldID;
+    
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Login_ButtonID.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    DBUtils.LogInUser(event, UsernameFieldID.getText(), PasswordFieldID.getText());
+                } catch (IOException e) { 
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+    
+}
