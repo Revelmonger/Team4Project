a357115471d2d5d674a91bd3d4d9e637257064c5
colejarrett <99043334+colejarrett@users.noreply.github.com>
2022-03-20 21:07:14 -0700

    new database
    ~cole

--- Installation Files/Database Export/Dump20220320.sql
+++ Installation Files/Database Export/Dump20220320.sql
@@ -37,6 +37,7 @@ CREATE TABLE `alerts` (
 
 LOCK TABLES `alerts` WRITE;
 /*!40000 ALTER TABLE `alerts` DISABLE KEYS */;
+INSERT INTO `alerts` VALUES (1,'allergy or sum');
 /*!40000 ALTER TABLE `alerts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -72,7 +73,7 @@ CREATE TABLE `appointments` (
 
 LOCK TABLES `appointments` WRITE;
 /*!40000 ALTER TABLE `appointments` DISABLE KEYS */;
-INSERT INTO `appointments` VALUES (1,13,1,1,'2022-03-18 16:53:33',1,5,'11','k','fe','1','efad',_binary '\0',_binary '\0'),(2,1,2,2,'2022-03-19 16:53:28',1,5,'22','ff','frd','2','gfd',_binary '',_binary '\0');
+INSERT INTO `appointments` VALUES (1,13,1,1,'2022-03-18 16:53:33',1,5,'11','k','fe','1','efad',_binary '\0',_binary '\0'),(2,1,2,2,'2022-03-19 16:53:28',1,5,'22','ff','frd','2','gfd',_binary '',_binary '\0'),(3,14,4,4,'2022-03-20 13:13:49',1,5,'3333','hjk','kfh','3','god',_binary '',_binary '\0');
 /*!40000 ALTER TABLE `appointments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -99,6 +100,7 @@ CREATE TABLE `diagnostic_reports` (
 
 LOCK TABLES `diagnostic_reports` WRITE;
 /*!40000 ALTER TABLE `diagnostic_reports` DISABLE KEYS */;
+INSERT INTO `diagnostic_reports` VALUES (1,1,13,1,'adsgfh');
 /*!40000 ALTER TABLE `diagnostic_reports` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -234,7 +236,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,13,1,1,1,'sdf',1,1),(2,1,1,2,2,'yo',3,1),(3,2,1,3,NULL,'gadg',4,1);
+INSERT INTO `orders` VALUES (1,13,1,1,1,'sdf',1,1),(2,1,1,2,2,'yo',3,1),(3,2,1,3,NULL,'gadg',4,1),(4,14,1,4,3,'fres',2,1);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -254,7 +256,7 @@ CREATE TABLE `patients` (
   `race` varchar(50) DEFAULT NULL,
   `ethnicity` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`patient_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -263,7 +265,7 @@ CREATE TABLE `patients` (
 
 LOCK TABLES `patients` WRITE;
 /*!40000 ALTER TABLE `patients` DISABLE KEYS */;
-INSERT INTO `patients` VALUES (1,'JOhn','doe','1111-11-11','male','White','Not Hispanic or Latino'),(2,'james','oejejfld','2222-02-22','female','White','Hispanic or Latino'),(13,'Cole','Jarrett','2001-08-29','male','White','Not Hispanic or Latino');
+INSERT INTO `patients` VALUES (1,'John','Doe','1112-10-11','male','White','Hispanic or Latino'),(2,'james','oejejfld','2222-02-22','female','White','Hispanic or Latino'),(13,'Cole','Jarrett','2001-08-29','male','White','Not Hispanic or Latino'),(14,'Jane','JLkji','3333-04-25','Female','White','Hispanic or Latino'),(15,'adf','hgfhf','2022-03-02','Female','Asian','Not Hispanic or Latino');
 /*!40000 ALTER TABLE `patients` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -306,9 +308,11 @@ DROP TABLE IF EXISTS `radiologists`;
 CREATE TABLE `radiologists` (
   `id` int NOT NULL AUTO_INCREMENT,
   `full_name` varchar(100) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `radiologists_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -317,7 +321,7 @@ CREATE TABLE `radiologists` (
 
 LOCK TABLES `radiologists` WRITE;
 /*!40000 ALTER TABLE `radiologists` DISABLE KEYS */;
-INSERT INTO `radiologists` VALUES (1,'test radiologist','test@radiologist.com');
+INSERT INTO `radiologists` VALUES (1,'test radiologist',6),(3,'jet set radio',9),(4,'fdasgbn',10);
 /*!40000 ALTER TABLE `radiologists` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -331,9 +335,11 @@ DROP TABLE IF EXISTS `referralmds`;
 CREATE TABLE `referralmds` (
   `id` int NOT NULL AUTO_INCREMENT,
   `full_name` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `referralmds_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -342,7 +348,7 @@ CREATE TABLE `referralmds` (
 
 LOCK TABLES `referralmds` WRITE;
 /*!40000 ALTER TABLE `referralmds` DISABLE KEYS */;
-INSERT INTO `referralmds` VALUES (1,'test doc','doc@doc.com');
+INSERT INTO `referralmds` VALUES (1,'test doc',3),(2,'b fdsvb',14),(3,'dgfhdfgh',15);
 /*!40000 ALTER TABLE `referralmds` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -386,7 +392,7 @@ CREATE TABLE `users` (
   `enabled` tinyint DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -395,7 +401,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin@example.com','test admin','admin','admin',1),(2,'user@example.com','test user','user','user',1),(3,'doc@example.com','test doc','doc','doc',1),(4,'receptionist@example.com','test desk','desk','desk',1),(5,'tech@example.com','test tech','tech','tech',1),(6,'radio@example.com','test radiologist','radio','radio',1);
+INSERT INTO `users` VALUES (1,'admin@example.com','test admin','admin','admin',1),(2,'user@example.com','test user','user','user',1),(3,'doc@example.com','test doc','doc','doc',1),(4,'receptionist@example.com','test desk','desk','desk',1),(5,'tech@example.com','test tech','tech','tech',1),(6,'radio@example.com','test radiologist','radio','radio',1),(7,'user','user user','user1','user1',1),(8,'dsg','hgfh','adf','d',NULL),(9,'gfgsdf','jet set radio','radio2','dr',1),(10,'adhfad','fdasgbn','adgbh','fed',NULL),(11,'dagcbadc','new doctor','new doc','docdocdoc',NULL),(12,'dsavasd','dafasdf','adfadf','df',NULL),(13,'vfad','vbfsd','fgb','jh',NULL),(14,'adfvc','b fdsvb','kjdfl','sses',NULL),(15,'bvnfdgh','dgfhdfgh','hsdfgfh','gfdhbvn',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -415,7 +421,7 @@ CREATE TABLE `users_roles` (
   KEY `role_fk_idx` (`role_id`),
   CONSTRAINT `role_fk` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
   CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=113 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=123 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -424,7 +430,7 @@ CREATE TABLE `users_roles` (
 
 LOCK TABLES `users_roles` WRITE;
 /*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
-INSERT INTO `users_roles` VALUES (1,1,1),(2,2,2),(4,4,4),(5,5,5),(6,6,6),(3,3,112);
+INSERT INTO `users_roles` VALUES (1,1,1),(2,2,2),(4,4,4),(5,5,5),(6,6,6),(3,3,112),(7,2,113),(8,4,114),(9,6,118),(10,6,119),(10,3,120),(14,3,121),(15,3,122);
 /*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -437,4 +443,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-18 21:42:32
+-- Dump completed on 2022-03-20 21:06:09
