c71658110f2ebea0be1bb981938541a89324b682
colejarrett <99043334+colejarrett@users.noreply.github.com>
2022-03-23 18:45:30 -0700

    Update ADMIN.java
    ~cole

--- Installation Files/DEMO application With Database/application/src/main/java/com/example/application/ADMIN.java
+++ Installation Files/DEMO application With Database/application/src/main/java/com/example/application/ADMIN.java
@@ -38,9 +38,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.effect.BoxBlur;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -910,14 +912,7 @@ private ScrollPane BlurBox;
 
                    
 
-                    String ImagePathStatement = "SELECT upload_path FROM db_ris.file_uploads WHERE order_id ='" + OrderID+ "'"; 
-                    statement5 = connectDB.createStatement();
-                    queryOutput = statement5.executeQuery(ImagePathStatement);
-
-                    while (queryOutput.next()) {
-                        String UploadPath = queryOutput.getString("upload_path");
-                      BufferedImage  img = ImageIO.read(new File(UploadPath));
-                    }
+                    
                  
 
 
@@ -980,8 +975,15 @@ button.setOnAction(new EventHandler<ActionEvent>() {
         horizontalline.setTranslateY(100);
 
   
-       
-Button showImage = new Button();
+        
+      /*  
+        Button showImage = new Button("Show Image");
+        showImage.setPrefHeight(42);
+        showImage.setPrefWidth(102);
+        showImage.setLayoutX(150);
+        showImage.setLayoutY(338);
+        showImage.setStyle("-fx-background-color: #566aff; -fx-text-fill: white;");*/
+
 
 
 
@@ -1054,8 +1056,32 @@ Button showImage = new Button();
           
                 anchorpane.getChildren().add(CancelButton);
                 anchorpane.getChildren().add(CreateDiagnosticReportButton);
+                
                 anchorpane.getChildren().add(ReportArea);
                 anchorpane.getChildren().add(ReportLabel);
+             String ImagePathStatement = "SELECT upload_path FROM db_ris.file_uploads WHERE order_id ='" + OrderID+ "'"; 
+        Statement statement;
+        try {
+            statement = connectDB.createStatement();
+            ResultSet queryOutput = statement.executeQuery(ImagePathStatement);
+
+        while (queryOutput.next()) {
+            String UploadPath = queryOutput.getString("upload_path");
+
+            /*ImageView imageView = new ImageView(new Image(UploadPath));
+
+            showImage.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+    
+                }*/
+            });
+        }
+
+        } catch (SQLException e3) {
+
+            e3.printStackTrace();
+        } 
                 
         Scene scene = new Scene(anchorpane, 800, 400);
 
