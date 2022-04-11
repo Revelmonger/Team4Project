dced3954f2ea6876cf4b3a8f7f70462ba0cd81b1
Revelmonger <cdmann0288@ung.edu>
2022-04-10 21:25:05 -0400

    Chase
    Cleaned Up Doc User

--- Installation Files/DEMO application With Database2/application/src/main/java/com/example/application/REFERRAL_DOCTOR_Referrals_Controller.java
+++ Installation Files/DEMO application With Database2/application/src/main/java/com/example/application/REFERRAL_DOCTOR_Referrals_Controller.java
@@ -7,36 +7,35 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+
 import java.util.ResourceBundle;
 
-import javax.persistence.criteria.Predicate.BooleanOperator;
+
 
 import com.example.application.Constructors.AllAlertsChoiceBoxController;
 import com.example.application.Constructors.Modalities;
 import com.example.application.Constructors.OrderStatuses;
-import com.example.application.Constructors.Patient;
+
 import com.example.application.Constructors.ReferralDoctor;
 import com.example.application.TableConstructors.PatientsAlertsTableController;
 import com.example.application.TableConstructors.TABLEReferralsTableController;
 
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Io;
+
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
+
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
+
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.CustomMenuItem;
+
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.MenuButton;
+
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -47,10 +46,9 @@ import javafx.scene.effect.BoxBlur;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BorderPane;
+
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
 import javafx.stage.Modality;
@@ -61,9 +59,7 @@ import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.application.*;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.BooleanBinding;
+
 import javafx.beans.value.ObservableValue;
 
 public class REFERRAL_DOCTOR_Referrals_Controller implements Initializable {
